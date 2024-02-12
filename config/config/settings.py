8cab26d21ce6a3afd5313530bd6d88fe08668350
Eowyn <eowyn@localhost.localdomain>
2024-02-12 17:10:49 +0300

    update file to redirect logged in/logged out users to homepage

--- config/config/settings.py
+++ config/config/settings.py
@@ -134,3 +134,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect logged in/logged out users to the homepage
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
